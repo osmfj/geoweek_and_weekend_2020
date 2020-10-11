c807575b7c86d9f6c1de50b769f62aa3f6ac04cc
Taro Matsuzawa <taro@georepublic.co.jp>
2020-10-11 19:20:47 +0900

    added music play

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -6,3 +6,34 @@ const map = new mapboxgl.Map({
   center: [139.767184, 35.680952],
   zoom: 17
 })
+
+const context = new AudioContext()
+let musicBuffer = null;
+let bufferSource = null;
+
+const loadSound = (url: string) => {
+  const request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.responseType = 'arraybuffer'
+
+  request.onload = () => {
+    context.decodeAudioData(request.response, (buffer) => {
+      musicBuffer = buffer
+      bufferSource = context.createBufferSource()
+      bufferSource.buffer = buffer
+      bufferSource.connect(context.destination)
+      bufferSource.start(context.currentTime + 0.100)
+    })
+  }
+  request.send()
+}
+
+const btn = document.getElementById('button')
+btn.addEventListener('click', () => {
+  if (musicBuffer == null) {
+    loadSound('https://archive.org/download/OTMN051/3_memory-of-the-cartridge.mp3')
+  } else {
+    !bufferSource || bufferSource.stop()
+  }
+}, false);
+
