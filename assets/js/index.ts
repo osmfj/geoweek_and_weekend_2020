e3dd810264a351d31f940a97a8f5c7ed2f771413
Taro Matsuzawa <taro@georepublic.co.jp>
2020-10-11 18:49:02 +0900

    added mapbox gl js into header

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,1 +1,8 @@
-console.log('hoge')
+import mapboxgl from 'mapbox-gl'
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'https://tile2.openstreetmap.jp/styles/osm-bright/style.json',
+  center: [139.767184, 35.680952],
+  zoom: 17
+})
